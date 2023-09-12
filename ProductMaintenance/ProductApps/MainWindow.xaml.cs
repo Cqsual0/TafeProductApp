50cd1b90a1321e99a1daa76ae8357d30e44aaf5d
001175397 <rory.douglass@student.tafesa.edu.au>
2023-09-12 16:02:19 +0930

    Documents for the task

---
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Product cProduct;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
+                cProduct.calTotalPayment();
+                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter data again", "Data Entry Error");
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            productTextBox.Text = "";
+            priceTextBox.Text = "";
+            quantityTextBox.Text = "";
+            totalPaymentTextBlock.Text = "";
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
