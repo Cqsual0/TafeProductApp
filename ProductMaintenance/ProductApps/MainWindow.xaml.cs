4ef2818b060fc497323cf913b7f7c249f0a8ca19
001175397 <rory.douglass@student.tafesa.edu.au>
2023-09-13 11:54:28 +0930

    Commit the branch feat_wrap1.1

---
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProductApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Product cProduct;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTotalCharge(TextBox totalChargeTextBlock, decimal totalPayment)
+        {
+            decimal totalCharge = totalPayment + 25m;
+            totalChargeTextBlock.Text = totalCharge.ToString("C2");
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
+                cProduct.calTotalPayment();
+                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+
+                // Calculate and display the total charge
+                CalculateTotalCharge(totalChargeTextBox, cProduct.TotalPayment);
+
+                // Calculate and display the wrap charge
+                UpdateWrapCharge(cProduct.TotalPayment);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter data again", "Data Entry Error");
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            productTextBox.Text = "";
+            priceTextBox.Text = "";
+            quantityTextBox.Text = "";
+            totalPaymentTextBlock.Text = "";
+            totalChargeTextBox.Text = "";
+            wrapChargeTextBox.Text = ""; // Clear the wrapChargeTextBox as well
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpdateWrapCharge(decimal totalPayment)
+        {
+            decimal wrapCharge = totalPayment + 25.00m + 5.00m; // TotalPayment + $25.00 + $5.00
+            wrapChargeTextBox.Text = wrapCharge.ToString("C2");
+        }
+
+        // This method will be triggered when the text in wrapChargeTextBox changes
+        private void wrapChargeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // If you want to do something when the text changes, you can add the logic here
+        }
+    }
+}
---
+++ ProductMaintenance/ProductApps/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApps
+{
+    class Product
+    {
+        private decimal price;
+        private int quantity;
+        private decimal totalPayment;
+        private decimal delivery;
+        private decimal wrapping;
+        private decimal gst;
+
+        private decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        private int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public decimal TotalPayment
+        {
+            get { return totalPayment; }
+            set { totalPayment = value; }
+        }
+
+
+        private decimal Delivery
+        {
+            get { return delivery; }
+            set { delivery = value; }
+        }
+
+
+        private decimal Wrapping
+        {
+            get { return wrapping; }
+            set { wrapping = value; }
+        }
+
+        private decimal GST
+        {
+            get { return gst; }
+            set { gst = value; }
+        }
+
+        //Constructor for Product
+        public Product(decimal price, int quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+
+        //Calculate the total payment
+        public void calTotalPayment()
+        {
+            TotalPayment = Price * Quantity;
+        }
+    }
+}
